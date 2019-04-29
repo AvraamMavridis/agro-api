2d1ecc67d92d4e9160c5be43274596bc7076c44e
AvraamMavridis <avraammauridis@gmail.com>
2019-04-29 11:06:12 +0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,46 @@
 plant_types = PlantType.create([
   {
+    id: 1,
     name: 'Sweet Potato',
     category: 'Roots',
   },
   {
+    id: 3,
     name: 'Beet',
     category: 'Roots',
   },
   {
+    id: 3,
     name: 'Onion',
     category: 'Roots',
   },
   {
+    id: 4,
     name: 'Carrot',
     category: 'Roots',
   },
   {
+    id: 5,
     name: 'Parsnip',
     category: 'Roots',
   },
   {
+    id: 6,
     name: 'Potato',
     category: 'Roots',
   },
   {
+    id: 7,
     name: 'Radish',
     category: 'Roots',
   },
   {
+    id: 8,
     name: 'Rutabaga',
     category: 'Roots',
   },
   {
+    id: 9,
     name: 'Turnip',
     category: 'Roots',
   },
