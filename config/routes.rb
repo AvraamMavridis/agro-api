464789ecacc87a907abd5ceaf561bb61bdff242a
AvraamMavridis <avraammauridis@gmail.com>
2019-05-19 06:24:21 +0200

    ci

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
 
-  resources :operation_type, only: [:index]
   resources :plant_type, only: [:index]
   resources :operation_type, only: [:index]
   resources :user, only: [:index, :create]
